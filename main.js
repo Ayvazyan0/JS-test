2fc20dae7b27878877b6676a26100c2f3fa74729
Ayvazyan <mailanoc.g@gmail.com>
2023-03-26 12:59:06 +0300

    Update

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function myf(){
-    document.getElementById('box').style.backgroundColor = 'yellow';
+    document.getElementById('box').style.backgroundColor = 'rgba(240, 175, 12, 0.5)';
+    document.getElementById('btn1').style.color = 'black';
+    document.getElementById('btn2').style.color = 'black';
+    document.getElementById('btn2').style.backgroundColor = 'rgba(240, 175, 12, 0.0)';
+    document.getElementById('btn1').style.backgroundColor = 'rgba(240, 175, 12, 0.0)';
+    document.getElementById('text').style.color = 'black';
 }
 
 function myff(){
     document.getElementById('box').style.backgroundColor = 'black';
+    document.getElementById('btn2').style.color = 'white';
+    document.getElementById('btn1').style.color = 'white';
+    document.getElementById('btn2').style.backgroundColor = 'black';
+    document.getElementById('btn1').style.backgroundColor = 'black';
+    document.getElementById('text').style.color = 'white';
 }
