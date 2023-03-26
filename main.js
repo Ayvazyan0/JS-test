95b3a36189c3877d18bbbe34ef65ce748372347b
Ayvazyan <mailanoc.g@gmail.com>
2023-03-26 14:39:24 +0300

    Update

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 function myf(){
-    document.getElementById('box').style.backgroundColor = 'rgba(240, 175, 12, 0.5)';
-    document.getElementById('btn1').style.color = 'black';
-    document.getElementById('btn2').style.color = 'black';
-    document.getElementById('btn2').style.backgroundColor = 'rgba(240, 175, 12, 0.0)';
-    document.getElementById('btn1').style.backgroundColor = 'rgba(240, 175, 12, 0.0)';
-    document.getElementById('text').style.color = 'black';
+    box.style.backgroundColor = 'rgba(240, 175, 12, 0.5)';
+    btn1.style = 'color:black; background-color:rgba(240, 175, 12, 0.0);'
+    btn2.style = 'color:black; background-color:rgba(240, 175, 12, 0.0);'
+    text.style.color = 'black';
 }
 
 function myff(){
-    document.getElementById('box').style.backgroundColor = 'black';
-    document.getElementById('btn2').style.color = 'white';
-    document.getElementById('btn1').style.color = 'white';
-    document.getElementById('btn2').style.backgroundColor = 'black';
-    document.getElementById('btn1').style.backgroundColor = 'black';
-    document.getElementById('text').style.color = 'white';
+    box.style.backgroundColor = 'black';
+    btn2.style = 'color:white; background-color:black;'
+    btn1.style = 'color:white; background-color:black;'
+    text.style.color = 'white';
 }
+
